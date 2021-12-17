83c06fb75411e6176ff58857ff8af67eee17d430
sally <2683527291@qq.com>
2021-12-17 10:07:48 +0800

    token:two

--- src/component/list.jsx
+++ src/component/list.jsx
@@ -3,42 +3,51 @@ import { List, Avatar, Col } from 'antd'
 import { Link, withRouter } from 'react-router-dom'
 import { UserOutlined } from '@ant-design/icons'
 import TopicTag from './topicTag'
-import { nav } from '../router/index'
 import Dayjs from './day'
 
 function ListComponent(props) {
-  const { loading, data, history } = props
-  const ifLoaded = () => {
-    if (nav[3].txt === '登录') {
-      history.push('/login')
-    }
-  }
+  const { loading, data } = props
   return (
     <List
       className="topic_list"
       loading={loading}
       dataSource={data}
       renderItem={(item) => {
-        // console.log(data)
         const {
           author, last_reply_at, good, tab, top, title, id,
         } = item
         const { avatar_url, loginname } = author
         return (
-          <List.Item onClick={ifLoaded}>
-            <Col xs={24} md={20} className="avatar">
-              <Link to={`/user/${loginname}`}>
-                <Avatar
-                  icon={<UserOutlined />}
-                  src={avatar_url}
-                  title={loginname}
-                />
-                <TopicTag tab={top ? 'top' : good ? 'good' : tab} />
-              </Link>
-              <Link to={`/topic/${id}`} style={{ marginLeft: 10 }}>
-                {title}
-              </Link>
-            </Col>
+          <List.Item>
+            {localStorage.getItem('token') ? (
+              <Col xs={24} md={20} className="avatar">
+                <Link to={`/user/${loginname}`}>
+                  <Avatar
+                    icon={<UserOutlined />}
+                    src={avatar_url}
+                    title={loginname}
+                  />
+                  <TopicTag tab={top ? 'top' : good ? 'good' : tab} />
+                </Link>
+                <Link to={`/topic/${id}`} style={{ marginLeft: 10 }}>
+                  {title}
+                </Link>
+              </Col>
+            ) : (
+              <Col xs={24} md={20} className="avatar">
+                <Link to="/login">
+                  <Avatar
+                    icon={<UserOutlined />}
+                    src={avatar_url}
+                    title={loginname}
+                  />
+                  <TopicTag tab={top ? 'top' : good ? 'good' : tab} />
+                </Link>
+                <Link to="/login" style={{ marginLeft: 10 }}>
+                  {title}
+                </Link>
+              </Col>
+            )}
             <Col xs={0} md={4} className="fromNow">
               <Dayjs date={last_reply_at} />
             </Col>
